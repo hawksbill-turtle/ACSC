fc71b8f7e90811ba58ee5910580aa5519b4be357
hawksbill-turtle <mjjang@rit.kaist.ac.kr>
2024-02-29 09:58:47 +0900

    update bed_bathing for train

--- assistive_gym/envs/bed_bathing_envs.py
+++ assistive_gym/envs/bed_bathing_envs.py
@@ -10,7 +10,7 @@ from .agents.human import Human
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 from ray.tune.registry import register_env
 
-robot_arm = 'left'
+robot_arm = 'right'
 human_controllable_joint_indices = human.right_arm_joints
 class BedBathingPR2Env(BedBathingEnv):
     def __init__(self):
